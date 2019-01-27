fd438db588e1da42c678d3ba555089824ddabd50
Lev Levin <combolev@gmail.com>
2019-01-27 18:58:05 +0300

    rewrite tests for List

--- HashTable/src/test/java/com/example/HashTable/ListTest.java
+++ HashTable/src/test/java/com/example/HashTable/ListTest.java
@@ -1,29 +1,34 @@
 package com.example.HashTable;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListTest {
 
-    @Test
-    void add() {
-        var list = new List();
+    private List list;
 
-        list.add("0", "I solemnly swear");
-        list.add("1", "that");
-        list.add("2", "I am up to no good");
-
-        assertEquals(list.find("0"), "I solemnly swear");
-        assertEquals(list.find("1"), "that");
-        assertEquals(list.find("2"), "I am up to no good");
-        assertNull(list.find("The Marauder’s Map"));
+    @BeforeEach
+    void initialize() {
+        list = new List();
     }
 
     @Test
-    void remove() {
-        var list = new List();
+    void Find_AfterAddingElements_FindsAll() {
+        list.add("0", "I solemnly swear");
+        list.add("1", "that");
+        list.add("2", "I am up to no good");
+        list.add("3", "(The Marauder’s Map)");
 
+        assertEquals("I solemnly swear", list.find("0"));
+        assertEquals("that", list.find("1"));
+        assertEquals("I am up to no good", list.find("2"));
+        assertEquals("(The Marauder’s Map)", list.find("3"));
+    }
+
+    @Test
+    void Find_AfterRemovingElements_NullIfRemoved() {
         list.add("2017", "chicken");
         list.add("2018", "dog");
         list.add("2019", "pig");
@@ -32,37 +37,26 @@ class ListTest {
         list.remove("2017");
         list.remove("2019");
 
+        assertNull(list.find("2017"));
         assertNull(list.find("2019"));
-        assertEquals(list.find("2018"), "dog");
-
-        list.add("2022", "tiger");
-        list.add("2024", "dragon");
-
-        assertNull(list.remove("2019"));
-        assertEquals(list.remove("2020"), "rat");
     }
 
     @Test
-    void find() {
-        var list = new List();
+    void Find_AfterRemovingElements_FindsNotRemoved() {
+        list.add("2017", "chicken");
+        list.add("2018", "dog");
+        list.add("2019", "pig");
+        list.add("2020", "rat");
 
-        list.add("19", "natural");
-        list.add("sqrt(2)", "irrational");
-        list.add("1+i", "complex");
+        list.remove("2017");
+        list.remove("2019");
 
-        assertNull(list.find("9/10"));
-        assertEquals(list.find("1+i"), "complex");
-        assertEquals(list.find("19"), "natural");
-
-        list.remove("19");
-
-        assertNull(list.find("19"));
+        assertEquals("rat", list.find("2020"));
+        assertEquals("dog", list.find("2018"));
     }
 
     @Test
-    void clear() {
-        var list = new List();
-
+    void Find_AfterClearingTable_Null() {
         list.add("1", "Out in the distance");
         list.add("2", "There's so much gold");
         list.add("3,", "The treasure that I've found");
