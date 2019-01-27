5ea5eb8cf538ca188350ea10f87113e2723acc06
Lev Levin <combolev@gmail.com>
2019-01-27 18:01:44 +0300

    Now HashTable can increase number of lists

--- HashTable/src/main/java/com/example/HashTable/HashTable.java
+++ HashTable/src/main/java/com/example/HashTable/HashTable.java
@@ -1,5 +1,7 @@
 package com.example.HashTable;
 
+import java.util.Vector;
+
 import static java.lang.Math.abs;
 
 /** Hash table containing String objects. */
@@ -9,10 +11,13 @@ public class HashTable {
     private int size;
 
     /** Container with pairs (key, value). */
-    private List[] container;
+    private Vector<List> container;
 
     /** The default max hash value. */
     private static final int DEFAULT_MAX_HASH = 10000;
+
+    /** The valid maximum of a value size / container.size() */
+    private static final float MAX_VALID_FILLING = (float) 0.75;
 
     HashTable() {
         this(DEFAULT_MAX_HASH);
@@ -23,9 +28,9 @@ public class HashTable {
             maxHash = DEFAULT_MAX_HASH;
         }
 
-        container = new List[maxHash];
+        container = new Vector<List>(maxHash);
         for (int i = 0; i < maxHash; i++) {
-            container[i] = new List();
+            container.add(new List());
         }
 
         size = 0;
@@ -52,7 +57,7 @@ public class HashTable {
      * @return the string corresponding to the key
      */
     public String get(String key) {
-        return container[abs(key.hashCode()) % container.length].find(key);
+        return container.get(abs(key.hashCode()) % container.size()).find(key);
     }
 
     /**
@@ -61,9 +66,13 @@ public class HashTable {
      */
     public String put(String key, String value) {
         String result = remove(key);
-        container[abs(key.hashCode()) % container.length].add(key, value);
+        container.get(abs(key.hashCode()) % container.size()).add(key, value);
         if (result == null) {
             size++;
+        }
+
+        if (size >= MAX_VALID_FILLING * container.size()) {
+            increaseSize(size * 2);
         }
 
         return result;
@@ -77,14 +86,31 @@ public class HashTable {
         if (contains(key))
             size--;
 
-        return container[abs(key.hashCode()) % container.length].remove(key);
+        return container.get(abs(key.hashCode()) % container.size()).remove(key);
     }
 
     /** Clears the hash table. */
     public void clear() {
-        for (int i = 0; i < container.length; i++) {
-            container[i].clear();
+        for (int i = 0; i < container.size(); i++) {
+            container.get(i).clear();
         }
+
+        container = new Vector<List>(DEFAULT_MAX_HASH);
+        for (int i = 0; i < DEFAULT_MAX_HASH; i++) {
+            container.add(new List());
+        }
+
         size = 0;
+    }
+
+    /** Increase a size of the container. */
+    private void increaseSize(int size) {
+        if (size <= container.size()) {
+            return;
+        }
+
+        while (container.size() < size) {
+            container.add(new List());
+        }
     }
 }
