7936f1fa674629b4f785c0fb7f6749ce2572ef55
Lev Levin <combolev@gmail.com>
2019-02-04 03:28:16 +0300

    add realization of Trie except Serializable methods

--- Trie/src/main/java/com/example/Trie/Trie.java
+++ Trie/src/main/java/com/example/Trie/Trie.java
@@ -1,6 +1,7 @@
 package com.example.Trie;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,6 +19,9 @@ public class Trie implements Serializable {
 
         /** A HashMap contains references to the next nodes. */
         private HashMap<Character, Node> next;
+
+        /** A number of elements passing through this node. */
+        int weight;
 
         private boolean isTerminal;
     }
@@ -40,7 +44,34 @@ public class Trie implements Serializable {
      * @return true if element wasn't in trie before adding
      */
     public boolean add(@NotNull String element) {
+        if (root == null) {
+            root = createNewBranch(element);
+            size++;
+            return true;
+        }
 
+        Node current = root;
+        Node next;
+        boolean result = false;
+
+        for (int i = 0; i < element.length(); i++) {
+            next = current.next.get(element.charAt(i));
+
+            if (next != null) {
+                current = next;
+            } else {
+                next = createNewBranch(element.substring(i + 1));
+                current.next.put(element.charAt(i), next);
+                result = true;
+                break;
+            }
+        }
+
+        if (result) {
+            size++;
+        }
+
+        return result;
     }
 
     /**
@@ -49,7 +80,20 @@ public class Trie implements Serializable {
      * @return true if an element is in a trie
      */
     public boolean contains(@NotNull String element) {
+        Node current = root;
+        Node next;
 
+        for (char symb: element.toCharArray()) {
+            next = current.next.get(symb);
+
+            if (next != null) {
+                current = next;
+            } else {
+                return false;
+            }
+        }
+
+        return current.isTerminal;
     }
 
     /**
@@ -59,7 +103,28 @@ public class Trie implements Serializable {
      * @return true if an element was in a trie before removing
      */
     public boolean remove(@NotNull String element) {
+        if (!contains(element)) {
+            return false;
+        }
 
+        Node current = root;
+        current.weight--;
+        Node next;
+
+        for (char symb: element.toCharArray()) {
+            next = current.next.get(symb);
+
+            if (next.weight > 1) {
+                next.weight--;
+                current = next;
+            } else {
+                current.next.remove(symb);
+                return true;
+            }
+        }
+        current.isTerminal = false;
+
+        return true;
     }
 
     /**
@@ -67,7 +132,7 @@ public class Trie implements Serializable {
      * @return number of containing strings
      */
     public int size() {
-
+        return size;
     }
 
     /**
@@ -77,7 +142,20 @@ public class Trie implements Serializable {
      * @return the number of strings starts with a prefix
      */
     public int howManyStartsWithPrefix(@NotNull String prefix) {
+        Node current = root;
+        Node next;
 
+        for (char symb: prefix.toCharArray()) {
+            next = current.next.get(symb);
+
+            if (next != null) {
+                current = next;
+            } else {
+                return 0;
+            }
+        }
+
+        return current.weight;
     }
 
     /**
@@ -102,8 +180,35 @@ public class Trie implements Serializable {
 
     }
 
-    /** Executes when a root of a trie is null. */
-    private Node addFirstElement(@NotNull String element) {
+    /** Creates a new branch in a trie */
+    private Node createNewBranch(@Nullable String element) {
+        Node first = new Node();
+        first.next = new HashMap<>();
+        first.weight = 1;
+        Node prev = first;
+
+        if (element != null) {
+            for (char symb : element.toCharArray()) {
+                Node node = new Node();
+                node.next = new HashMap<>();
+                node.weight = 1;
+
+                prev.next.put(symb, node);
+                prev = node;
+            }
+        }
+
+        prev.isTerminal = true;
+        return first;
+    }
+
+    /**
+     * Codes a subtree. Used only as a part of serialize.
+     * @param node -- a root of subtree
+     * @param nodeCode -- a minimum positive value of a non used node code
+     * @return a minimum positive value of a non used node code after coding
+     */
+    private int codeSubtree(@NotNull OutputStream out, Node node, int nodeCode) throws IOException {
 
     }
 }
