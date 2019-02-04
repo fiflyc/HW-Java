15bd69efd5ef227504708c917588fc9d99b61348
Lev Levin <combolev@gmail.com>
2019-02-04 17:17:00 +0300

    codestyle fix

--- Trie/src/main/java/com/example/Trie/Trie.java
+++ Trie/src/main/java/com/example/Trie/Trie.java
@@ -4,9 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 /**
