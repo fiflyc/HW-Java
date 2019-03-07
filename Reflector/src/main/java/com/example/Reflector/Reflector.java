86654ed7cb7c67de1390e54ca90dee53288cff86
Lev Levin <combolev@gmail.com>
2019-03-08 01:30:30 +0300

    add realization of method printAllMethods()

--- Reflector/src/main/java/com/example/Reflector/Reflector.java
+++ Reflector/src/main/java/com/example/Reflector/Reflector.java
@@ -16,6 +16,10 @@ public class Reflector {
             printClassHeading(someClass, writer);
             writer.write(" {\n\n");
             printAllFields(someClass, writer);
+            writer.write("\n");
+            printAllMethods(someClass, writer);
+            writer.write("}\n");
+
             writer.flush();
         }
     }
@@ -63,6 +67,45 @@ public class Reflector {
             writer.write(current.getGenericType().getTypeName().replace('$', '.'));
             writer.write(" ");
             writer.write(current.getName());
+            writer.write(";\n");
+        }
+    }
+
+    /**
+     * Prints all methods of class (private, public etc) with generic types.
+     * Also prints all throwing exception.
+     * @param someClass class for printing
+     * @param writer output writer
+     * @throws IOException
+     */
+    private static void printAllMethods(Class<?> someClass, OutputStreamWriter writer) throws IOException {
+        for (var current: someClass.getDeclaredMethods()) {
+            writer.write("\t");
+            if (current.getModifiers() != 0) {
+                writer.write(Modifier.toString(current.getModifiers()));
+                writer.write(" ");
+            }
+            writer.write(current.getGenericReturnType().getTypeName().replace('$', '.'));
+            writer.write(" ");
+            writer.write(current.getName());
+            writer.write("(");
+            if (current.getGenericParameterTypes().length > 0) {
+                var parameters = current.getGenericParameterTypes();
+                for (int i = 0; i < parameters.length - 1; i++) {
+                    writer.write(parameters[i].getTypeName().replace('$', '.'));
+                    writer.write(" ");
+                }
+                writer.write(parameters[parameters.length - 1].getTypeName().replace('$', '.'));
+            }
+            writer.write(")");
+            if (current.getGenericExceptionTypes().length > 0) {
+                var exceptions = current.getGenericExceptionTypes();
+                for (int i = 0; i < exceptions.length - 1; i++) {
+                    writer.write(exceptions[i].getTypeName().replace('$', '.'));
+                    writer.write(" ");
+                }
+                writer.write(exceptions[exceptions.length - 1].getTypeName().replace('$', '.'));
+            }
             writer.write(";\n");
         }
     }
