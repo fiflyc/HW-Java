a55d8a8fe917dc2b226d932e0fe6e9d74bcb7575
Lev Levin <combolev@gmail.com>
2019-03-13 17:21:43 +0300

    add realization of class LinkedHashMap

--- LinkedHashMap/src/main/java/com/example/LinkedHashMap/LinkedHashMap.java
+++ LinkedHashMap/src/main/java/com/example/LinkedHashMap/LinkedHashMap.java
@@ -1,30 +1,38 @@
 package com.example.LinkedHashMap;
 
-import cucumber.api.java.ca.I;
+import com.twelvemonkeys.imageio.metadata.AbstractEntry;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.AbstractMap;
+import java.util.Map;
 
 public class LinkedHashMap<K, V> extends AbstractMap {
 
-    static public class Set<E> {
+    static private class Entry<K, V> extends AbstractEntry {
 
-        static private class Node<E> {
-            private E value;
-            private Node next;
-            private Node prev;
-
-            Node(@NotNull E value) {
-                this.value = value;
-                prev = null;
-                next = null;
-            }
+        protected Entry(Object o, Object o1) {
+            super(o, o1);
         }
+    }
+
+    static private class Node<E> {
+        private E value;
+        private Node next;
+        private Node prev;
+
+        Node(@NotNull E value) {
+            this.value = value;
+            prev = null;
+            next = null;
+        }
+    }
+
+    public class Set<E> extends java.util.AbstractSet {
 
         private class Iterator implements java.util.Iterator<E> {
 
-            private Set.Node<E> current;
+            private Node<E> current;
 
             private Iterator(@Nullable Node<E> node) {
                 current = node;
@@ -42,12 +50,10 @@ public class LinkedHashMap<K, V> extends AbstractMap {
         }
 
         private Node<E> head;
-        private Node<E> tail;
         private int size;
 
         private Set() {
             head = null;
-            tail = null;
             size = 0;
         }
 
@@ -55,44 +61,99 @@ public class LinkedHashMap<K, V> extends AbstractMap {
             var node = new Node<>(value);
             size++;
 
-            if (tail == null) {
-                tail = node;
+            if (head == null) {
                 head = node;
                 return;
             }
 
-            tail.next = node;
-            node.prev = tail;
-            tail = node;
+            head.next = node;
+            node.prev = head;
+            head = node;
         }
 
+        @Override
         public Iterator iterator() {
             return new Iterator(head);
         }
 
+        @Override
         public int size() {
             return size;
         }
+
+        @Override
+        public void clear() {
+            head = null;
+        }
+    }
+
+    private List[] container;
+    private int size;
+    private List.Node firstByOrder;
+    private List.Node lastByOrder;
+    private static final int DEFAULT_CONTAINER_LENGTH = 10000;
+
+    LinkedHashMap() {
+        this(DEFAULT_CONTAINER_LENGTH);
+    }
+
+    LinkedHashMap(int capacity) {
+        if (capacity > 0) {
+            container = new List[capacity];
+        } else {
+            container = new List[DEFAULT_CONTAINER_LENGTH];
+        }
+
+        for (int i = 0; i < container.length; i++) {
+            container[i] = new List<K, V>();
+        }
+
+        firstByOrder = null;
+        lastByOrder = null;
+        size = 0;
+
     }
 
     @Override
-    public Object put(Object key, Object value) {
+    public Object put(@NotNull Object key, @NotNull Object value) {
+        Object result = container[getHash((K) key)].find(key);
+        if (result != null) {
+            container[getHash((K) key)].remove(key);
+        } else {
+            size++;
+        }
 
-        return null;
+        lastByOrder = container[getHash((K) key)].add(key, value, lastByOrder);
+
+        return result;
     }
 
     @Override
-    public V remove(Object key) {
-        return null;
+    public V remove(@NotNull Object key) {
+        Object result = container[getHash((K) key)].find(key);
+        if (result != null) {
+            size--;
+        }
+
+        return (V) result;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
-    public Set<Entry> entrySet() {
-        return null;
+    public java.util.Set entrySet() {
+        var set = new Set<Entry<K, V>>();
+        for (var current = firstByOrder; current != null; current = current.nextByOrder()) {
+            set.put(new Entry<>(current.getKey(), current.getValue()));
+        }
+
+        return set;
+    }
+
+    private int getHash(@NotNull K key) {
+        return key.hashCode() % container.length;
     }
 }
