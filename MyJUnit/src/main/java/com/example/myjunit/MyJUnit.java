0daa07fbb8e011e6c0df1738ae2f4cf243c769ec
Lev Levin <combolev@gmail.com>
2019-06-02 07:50:15 +0300

    Add MyJUnit class

---
+++ MyJUnit/src/main/java/com/example/myjunit/MyJUnit.java
@@ -0,0 +1,128 @@
+package com.example.myjunit;
+
+import org.junit.*;
+
+import javax.tools.ToolProvider;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MyJUnit {
+
+    /** Thread Pool what takes tasks "run testes". */
+    private static ExecutorService threadPool;
+
+    /** Get directory with testes for executing. */
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            return;
+        }
+
+        threadPool = Executors.newFixedThreadPool(30);
+        File dir = new File(args[0]);
+        if (dir.isFile()) {
+            runTestsFromFile(dir);
+        } else {
+            for (var file: dir.listFiles()) {
+                runTestsFromFile(file);
+            }
+        }
+    }
+
+    /** Run testes from file. */
+    public static void runTestsFromFile(File file) {
+        if (threadPool == null) {
+            threadPool = Executors.newFixedThreadPool(30);
+        }
+
+        var compiler = ToolProvider.getSystemJavaCompiler();
+        compiler.run(null, null, null,
+                "-d",
+                "./out/production/classes/com/example/myjunit",
+                file.getAbsolutePath());
+
+        try {
+            var loader = new URLClassLoader(new URL[]{file.toURI().toURL()});
+            Class<?> cl = Class.forName(file.getAbsolutePath(), true, loader);
+
+            var beforeAll = new ArrayList<Method>();
+            var afterAll = new ArrayList<Method>();
+            var beforeEach = new ArrayList<Method>();
+            var afterEach = new ArrayList<Method>();
+            var testes = new ArrayList<Method>();
+
+            for (var method: cl.getMethods()) {
+                for (var annotation: method.getDeclaredAnnotations()) {
+                    if (annotation.getClass().equals(BeforeClass.class)) {
+                        beforeAll.add(method);
+                        break;
+                    } else if (annotation.getClass().equals(AfterClass.class)) {
+                        afterAll.add(method);
+                        break;
+                    } else if (annotation.getClass().equals(Before.class)) {
+                        beforeEach.add(method);
+                        break;
+                    } else if (annotation.getClass().equals(After.class)) {
+                        afterEach.add(method);
+                        break;
+                    } else if (annotation.getClass().equals(Test.class)) {
+                        testes.add(method);
+                        break;
+                    }
+                }
+            }
+
+            executeTestes(beforeAll, null, null);
+            executeTestes(testes, beforeEach, afterEach);
+            executeTestes(afterAll, null, null);
+        } catch (MalformedURLException e) {
+            System.out.println("Failed while creating class loader");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Failed while loading class from file " + file.getAbsolutePath());
+        }
+    }
+
+    /**
+     * Execute testes in thread Pool.
+     * Locks until they executing.
+     * @param testes testes for executing
+     * @param beforeEach testes executing before each testes
+     * @param afterEach testes executing after each testes
+     */
+    private static void executeTestes(ArrayList<Method> testes, ArrayList<Method> beforeEach, ArrayList<Method> afterEach) {
+        var latch = new CountDownLatch(testes.size());
+
+        for (var test: testes) {
+            threadPool.submit(() -> {
+                try {
+                    for (var before: beforeEach) {
+                        before.invoke(new Object[] {});
+                    }
+                    test.invoke(new Object[] {});
+                    for (var after: afterEach) {
+                        after.invoke(new Object[] {});
+                    }
+                } catch (IllegalAccessException e) {
+                    System.out.println("Illegal access for the test " + test.getName());
+                } catch (InvocationTargetException e) {
+                    System.out.println("Test " + test.getName() + "has some arguments");
+                }
+
+                latch.countDown();
+            });
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            /* Do nothing. */
+        }
+    }
+}
